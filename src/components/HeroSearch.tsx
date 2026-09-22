82d2de67e4f6e9c7933965556da4e6ecacadefab
agenciacaen <agenciacaen@gmail.com>
2026-09-22 00:11:20 -0300

    fix: definitive scroll lock when modal is open

--- src/components/HeroSearch.tsx
+++ src/components/HeroSearch.tsx
@@ -80,11 +80,24 @@ function HeroSearchContent({ variant = 'horizontal', theme = 'dark', projectSlug
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = 'hidden'
+      document.body.style.position = 'fixed'
+      document.body.style.top = `-${window.scrollY}px`
+      document.body.style.width = '100%'
     } else {
+      const scrollY = document.body.style.top
       document.body.style.overflow = ''
+      document.body.style.position = ''
+      document.body.style.top = ''
+      document.body.style.width = ''
+      if (scrollY) {
+        window.scrollTo(0, parseInt(scrollY || '0') * -1)
+      }
     }
     return () => {
       document.body.style.overflow = ''
+      document.body.style.position = ''
+      document.body.style.top = ''
+      document.body.style.width = ''
     }
   }, [isOpen])
 
